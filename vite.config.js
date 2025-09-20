d01ad5099f223e05f92f936b75ff471df8afc694
STEP3DLab <projects.step3d@gmail.com>
2025-09-20 08:08:53 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+//https://step3dlab.github.io/AT.R22//
 export default defineConfig({
   plugins: [react()],
   server: {
