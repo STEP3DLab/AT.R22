131701954260a23db4d76cac21a9f779cf62747f
STEP3DLab <projects.step3d@gmail.com>
2025-09-20 08:52:00 +0300

    Set Vite base to relative path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AT.R22/',   // ← имя репозитория ТОЧНО с тем же регистром
+  base: './',
   build: {
     outDir: 'docs',
     emptyOutDir: true,
