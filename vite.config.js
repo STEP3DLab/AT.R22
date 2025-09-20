c73d28168092ff05520a7e1c167ced3ab3dc9c80
STEP3DLab <projects.step3d@gmail.com>
2025-09-20 08:28:09 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AT.R22/',
-  server: {
-    port: 5173,
-    host: true,
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
+  base: '/AT.R22/',   // ← имя репозитория ТОЧНО с тем же регистром
 })
